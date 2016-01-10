d8645e1a8d30ff2ddbc65d1cd82d7c36972cf2eb
JerNaJa <jer_0220@hotmail.com>
2016-01-10 11:52:19 +0700

    ออก UI หน้า DetailActivity

---
+++ app/src/main/java/kmutnb/rattanawichai/chaturaphon/masiterung_traffic/DetailActivity.java
@@ -0,0 +1,13 @@
+package kmutnb.rattanawichai.chaturaphon.masiterung_traffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
