a010565092a36aaa453bdcc1c2bd4ce42818816c
JerNaJa <jer_0220@hotmail.com>
2016-01-10 13:10:16 +0700

    Start Detail

--- app/src/main/java/kmutnb/rattanawichai/chaturaphon/masiterung_traffic/MainActivity.java
+++ app/src/main/java/kmutnb/rattanawichai/chaturaphon/masiterung_traffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private void listViewController() {
         //Setup Array
         //For Icon
-        int[] iconInts = {R.drawable.traffic_01,R.drawable.traffic_02,R.drawable.traffic_03,
+        final int[] iconInts = {R.drawable.traffic_01,R.drawable.traffic_02,R.drawable.traffic_03,
                 R.drawable.traffic_04,R.drawable.traffic_05,R.drawable.traffic_06,
                 R.drawable.traffic_07,R.drawable.traffic_08,R.drawable.traffic_09,
                 R.drawable.traffic_10,R.drawable.traffic_11,R.drawable.traffic_12,
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.traffic_19,R.drawable.traffic_20};
 
         //For Title
-        String[] titleStrings = new String[20];
+        final String[] titleStrings = new String[20];
         titleStrings[0] = "หัวข้อที่ 1";
         titleStrings[1] = "หัวข้อที่ 2";
         titleStrings[2] = "หัวข้อที่ 3";
@@ -73,6 +74,24 @@ public class MainActivity extends AppCompatActivity {
 
         MyAdepter objMyAdepter = new MyAdepter(MainActivity.this, iconInts, titleStrings, detailStrings);
         trafficListView.setAdapter(objMyAdepter);
+
+        //Active When Click ListView
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Intent to DetailActivity
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class);
+
+                //putExtra
+                objIntent.putExtra("Title", titleStrings[position]);
+                objIntent.putExtra("Image", iconInts[position]);
+                objIntent.putExtra("Index", position);
+
+                startActivity(objIntent);
+
+            } //event
+        });
 
     }//listViewController
 
