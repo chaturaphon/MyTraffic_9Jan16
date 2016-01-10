567739cfcca14b33738266e2bc6c66a748a36105
JerNaJa <jer_0220@hotmail.com>
2016-01-10 10:51:10 +0700

    สร้าง MyAdeptor แล้ว

--- app/src/main/java/kmutnb/rattanawichai/chaturaphon/masiterung_traffic/MainActivity.java
+++ app/src/main/java/kmutnb/rattanawichai/chaturaphon/masiterung_traffic/MainActivity.java
@@ -1,13 +1,68 @@
 package kmutnb.rattanawichai.chaturaphon.masiterung_traffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        // Button Controller
+        buttonController();
+
+        //ListView Controller
+        listViewController();
+
+    }// Manu Method
+
+    private void listViewController() {
+        //Setup Array
+        //For Icon
+        int[] iconInts = {};
+
+    }//listViewController
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.mosquito);
+                buttonMediaPlayer.start();
+
+                // Intent to ActionView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/YBHQbu5rbdQ"));
+                startActivity(objIntent);
+
+            } // event ทำงานเมื่อคลิก
+        }); // event
+
+    } // buttonContronller
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
     }
-}
+
+}// Main Class นี่คือคลาสหลัก
